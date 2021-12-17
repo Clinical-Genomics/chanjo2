aeba244a7f7d2a1f88c54b03bb4d37e65d76c215
Vincent Janvid <vincent.janvid@scilifelab.se>
2021-12-17 11:16:35 +0100

    Removed my demo

--- src/chanjo2/main.py
+++ src/chanjo2/main.py
@@ -52,15 +52,7 @@ app.include_router(
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-    load_demo()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to Chanjo2: A strangely named app about to be reborn."}
-
-def load_demo():
-    """Load some dummy data into a test instance of chanjo2"""
-    with Session(engine) as session:
-        individual = Individual(individual_id="test_individual_exom",coverage_file_path="/Users/vincent.janvid/dev/chanjo2/tests/test_data/HG00152.alt_bwamem_GRCh38DH.20150826.GBR.exome.d4")
-        session.add(individual)
-        session.commit()
