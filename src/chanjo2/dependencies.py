d71bdf8c86769601b20a00655fca029b8871387e
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2022-02-14 23:08:34 +0100

    Use .env file

--- src/chanjo2/dependencies.py
+++ src/chanjo2/dependencies.py
@@ -1,6 +1,9 @@
+import os
 from sqlmodel import Session, create_engine
 
-mysql_url = f"mysql://root:my_secret_pw@d4database:3306/d4files"
+root_password = os.getenv('MYSQL_ROOT_PASSWORD')
+
+mysql_url = f"mysql://root:{root_password}@d4database:3306/d4files"
 
 engine = create_engine(mysql_url, echo=True)
 
