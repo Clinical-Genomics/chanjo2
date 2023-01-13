12b32c6d87516ee83c5680250050493e91980d90
Chiara Rasi <rasi.chiara@gmail.com>
2023-01-13 08:35:08 +0100

    Reformat code with black

--- tests/src/chanjo2/test_main.py
+++ tests/src/chanjo2/test_main.py
@@ -6,7 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 ### Mock a database session
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
