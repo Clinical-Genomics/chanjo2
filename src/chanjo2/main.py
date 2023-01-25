a391a7217965c9d8292fb7dcbf80c34b2b6ce964
Chiara Rasi <rasi.chiara@gmail.com>
2023-01-25 11:00:51 +0100

    Added colored logs

--- src/chanjo2/main.py
+++ src/chanjo2/main.py
@@ -1,13 +1,19 @@
+import logging
 from pathlib import Path
 from typing import List, Literal, Optional
 
+import coloredlogs
 from chanjo2 import __version__
 from chanjo2.dbutil import engine
 from fastapi import Depends, FastAPI, HTTPException, Query, status
 from pydantic import BaseModel
 from sqlmodel import Field, Session, SQLModel, select
 
+from .dbutil import DEMO_DB
 from .endpoints import individuals, regions
+
+LOG = logging.getLogger(__name__)
+coloredlogs.install(level="INFO")
 
 
 class CoverageInterval(BaseModel):
@@ -43,6 +49,8 @@ app.include_router(
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+    if str(engine.url) == DEMO_DB:
+        LOG.warning("Running a demo instance of Chanjo2")
 
 
 @app.get("/")
