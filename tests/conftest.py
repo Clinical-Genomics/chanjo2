5423d80f7266bf16f0a06ad13bbe4696ea8d7db5
Chiara Rasi <rasi.chiara@gmail.com>
2023-01-26 10:35:00 +0100

    Added a test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from chanjo2.main import app, create_db_and_tables, engine
+from chanjo2.main import Base, app, create_db_and_tables, engine
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,9 +7,16 @@ from sqlalchemy.orm import sessionmaker
 TEST_DB = "sqlite:///./test.db"
 
 
+@pytest.fixture(name="testdb")
+def testdb_fixture():
+    return TEST_DB
+
+
 @pytest.fixture(name="session")
 def session_fixture():
     engine = create_engine(TEST_DB, connect_args={"check_same_thread": False})
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     return sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
@@ -23,4 +30,5 @@ def client_fixture(session):
             db.close()
 
     app.dependency_overrides[create_db_and_tables] = override_get_db
+
     return TestClient(app)
--- tests/src/chanjo2/test_main.py
+++ tests/src/chanjo2/test_main.py
@@ -1,4 +1,7 @@
 from chanjo2 import __version__
+from sqlalchemy import create_engine, inspect
+
+DB_TABLES = ["cases", "interval_tag", "intervals", "samples", "tags"]
 
 
 def test_heartbeat(client):
@@ -6,3 +9,16 @@ def test_heartbeat(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": f"Chanjo2 v{__version__} is up and running!"}
+
+
+def test_create_db_and_tables(session, testdb):
+    """Test that tables are created correctly when app starts up"""
+    # Given a running instance of Chanjo2 (client fixture)
+
+    # WHEN connecting to the same database used by the test app
+    engine2 = create_engine(testdb, connect_args={"check_same_thread": False})
+
+    # THEN the expected tables should be found
+    insp = inspect(engine2)
+    for table in DB_TABLES:
+        assert insp.has_table(table)
