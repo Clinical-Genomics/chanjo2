fd71044f9d4f32a1a3ea88d9847507d37f1fe0b3
Chiara Rasi <rasi.chiara@gmail.com>
2023-01-27 08:35:46 +0100

    WHEN and THEN in test

--- tests/src/chanjo2/test_main.py
+++ tests/src/chanjo2/test_main.py
@@ -2,12 +2,16 @@ from chanjo2 import __version__
 from sqlalchemy import create_engine, inspect
 
 DB_TABLES = ["cases", "interval_tag", "intervals", "samples", "tags"]
+SUCCESS_CODE = 200
 
 
 def test_heartbeat(client):
     """Test the function that returns a message if server is running"""
+    # WHEN user makes a call to the heatbeat endpoint
     response = client.get("/")
-    assert response.status_code == 200
+    # THEN it should return success
+    assert response.status_code == SUCCESS_CODE
+    # AND the expected message
     assert response.json() == {"message": f"Chanjo2 v{__version__} is up and running!"}
 
 
