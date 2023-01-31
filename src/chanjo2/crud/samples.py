124cae6f0324cb3767aef2daac864001c7bdc694
Chiara Rasi <rasi.chiara@gmail.com>
2023-01-31 13:09:15 +0100

    Add return hunts to samples crud

--- src/chanjo2/crud/samples.py
+++ src/chanjo2/crud/samples.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List, Union
 
 from chanjo2.models import pydantic_models, sql_models
 from sqlalchemy.orm import Session
@@ -7,17 +8,17 @@ LOG = logging.getLogger("uvicorn.access")
 
 
 ### Case utils
-def get_cases(db: Session, skip: int = 0, limit: int = 100):
+def get_cases(db: Session, skip: int = 0, limit: int = 100) -> List[pydantic_models.CaseRead]:
     """Return all cases"""
     return db.query(sql_models.Case).offset(skip).limit(limit).all()
 
 
-def get_case(db: Session, case_name: str):
+def get_case(db: Session, case_name: str) -> pydantic_models.CaseRead:
     """Return a specific case by providing its ID"""
     return db.query(sql_models.Case).filter(sql_models.Case.name == case_name).first()
 
 
-def create_case(db: Session, case: pydantic_models.CaseCreate):
+def create_case(db: Session, case: pydantic_models.CaseCreate) -> pydantic_models.CaseRead:
     """Create a case"""
     db_sample = sql_models.Case(name=case.name, display_name=case.display_name)
     db.add(db_sample)
@@ -27,29 +28,27 @@ def create_case(db: Session, case: pydantic_models.CaseCreate):
 
 
 ### Sample utils
-def get_samples(db: Session, skip: int = 0, limit: int = 100):
+def get_samples(db: Session, skip: int = 0, limit: int = 100) -> List[pydantic_models.SampleRead]:
     """Return all samples"""
     return db.query(sql_models.Sample).offset(skip).limit(limit).all()
 
 
-def get_case_samples(db: Session, case_id: int):
+def get_case_samples(db: Session, case_id: int) -> List[pydantic_models.SampleRead]:
     """Return all samples for a given case ID"""
     return db.query(sql_models.Sample).filter(sql_models.Sample.id == case_id)
 
 
-def get_sample(db: Session, sample_id: int):
+def get_sample(db: Session, sample_id: int) -> pydantic_models.SampleRead:
     """Return a specific sample by its ID"""
     return db.query(sql_models.Sample).filter(sql_models.Sample.id == sample_id).first()
 
 
-def create_case_sample(db: Session, sample: pydantic_models.SampleCreate):
+def create_case_sample(
+    db: Session, sample: pydantic_models.SampleCreate
+) -> Union[pydantic_models.SampleRead, None]:
     """Create a sample"""
     # Check if sample's case exists first
-    case_obj = (
-        db.query(sql_models.Case)
-        .filter(sql_models.Case.name == sample.case_name)
-        .first()
-    )
+    case_obj = db.query(sql_models.Case).filter(sql_models.Case.name == sample.case_name).first()
     if not case_obj:
         return
 
