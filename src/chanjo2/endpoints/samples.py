a0ccb5670ec3d6bcd136514c6f977a1b6ca8c290
Chiara Rasi <rasi.chiara@gmail.com>
2023-02-06 10:51:07 +0100

    black

--- src/chanjo2/endpoints/samples.py
+++ src/chanjo2/endpoints/samples.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 from typing import List
 
-from chanjo2.crud.samples import create_case_sample, get_case_samples, get_sample, get_samples
+from chanjo2.crud.samples import (
+    create_case_sample,
+    get_case_samples,
+    get_sample,
+    get_samples,
+)
 from chanjo2.dbutil import get_session
 from chanjo2.models import pydantic_models, sql_models
 from chanjo2.models.pydantic_models import SampleCreate, SampleRead
@@ -48,5 +53,7 @@ def read_samples_for_case(case_name: str, db: Session = Depends(get_session)):
 def read_sample(sample_name: str, db: Session = Depends(get_session)):
     db_sample = get_sample(db, sample_name=sample_name)
     if db_sample is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Sample not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Sample not found"
+        )
     return db_sample
