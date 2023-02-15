372f45f01e24b7908e17a38856d49de52c96da3c
Chiara Rasi <rasi.chiara@gmail.com>
2023-02-15 12:17:05 +0100

    Add test for queries for coverage over an entire chromosome

--- tests/src/chanjo2/endpoints/test_intervals.py
+++ tests/src/chanjo2/endpoints/test_intervals.py
@@ -31,7 +31,7 @@ def test_read_single_interval(
     # WHEN using a query for the coverage over a genomic interval in a local D4 file
     interval_query["coverage_file_path"] = real_coverage_path
 
-    # THEN a request to the read_single_interval should return 404 error
+    # THEN a request to the read_single_interval should be successful
     response = client.get(interval_endpoint, params=interval_query)
     assert response.status_code == status.HTTP_200_OK
 
@@ -39,7 +39,36 @@ def test_read_single_interval(
     result = response.json()
     coverage_data = CoverageInterval(**result)
     assert coverage_data.mean_coverage > 0
-    # together with the queries interval
+    # together with the queried interval
     assert coverage_data.chromosome
     assert coverage_data.start
     assert coverage_data.end
+
+
+def test_read_single_chromosome(
+    client: TestClient,
+    real_coverage_path: str,
+    interval_endpoint: str,
+    interval_query: dict,
+):
+    """Test the function that returns the coverage over an entire chsomosome of a D4 file."""
+
+    # GIVEN an interval query without start and and  coordinates
+    interval_query.pop("start")
+    interval_query.pop("end")
+
+    # WHEN using the query for the coverage over a local D4 file
+    interval_query["coverage_file_path"] = real_coverage_path
+
+    # THEN a request to the read_single_intervalshould be successful
+    response = client.get(interval_endpoint, params=interval_query)
+    assert response.status_code == status.HTTP_200_OK
+
+    # AND the mean coverage over the entire chromosome should be present in the result
+    result = response.json()
+    coverage_data = CoverageInterval(**result)
+    assert coverage_data.mean_coverage > 0
+    # together with the queried interval
+    assert coverage_data.chromosome
+    assert coverage_data.start is None
+    assert coverage_data.end is None
