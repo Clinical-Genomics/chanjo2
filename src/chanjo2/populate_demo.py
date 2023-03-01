e9639aaf55898451b8535d2cf9ae0dfe98311e40
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-01 09:17:24 +0100

    Update src/chanjo2/populate_demo.py
    Co-authored-by: Henrik Stranneheim <henrik.stranneheim@scilifelab.se>

--- src/chanjo2/populate_demo.py
+++ src/chanjo2/populate_demo.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 db: sessionmaker = next(get_session())
 
-DEMO_CASE = {"name": "internal_id", "display_name": "643594"}
+DEMO_CASE: Dict[str, str] = {"name": "internal_id", "display_name": "643594"}
 DEMO_SAMPLE = {
     "name": "ADM1059A2",
     "display_name": "NA12882",
