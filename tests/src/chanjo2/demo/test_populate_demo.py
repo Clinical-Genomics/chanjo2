70b630b66c1fa06ee74ad0398da0474a643e2d98
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-01 08:31:46 +0100

    Fixed the test

--- tests/src/chanjo2/demo/test_populate_demo.py
+++ tests/src/chanjo2/demo/test_populate_demo.py
@@ -1,21 +1,19 @@
-import time
-
-from chanjo2.demo.populate_demo import load_demo_data
+from chanjo2.crud.cases import get_cases
+from chanjo2.dbutil import get_session
 from chanjo2.main import app
-from chanjo2.models.pydantic_models import Case
+from chanjo2.models.sql_models import Case
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import sessionmaker
+
+client = TestClient(app)
+db: sessionmaker = next(get_session())
 
 
 def test_load_demo_data(cases_endpoint: str):
     """Test the function that adds demo data"""
 
-    # GIVEN a client of a read demo app
+    # WHEN the app is launched
     with TestClient(app) as client:
-        # After the startup event
-        time.sleep(1)
-        # THEN the cases endpoint
-        response = client.get(cases_endpoint)
-        result: list = response.json()
-        # SHOULD return 1 case
-        assert len(result) == 1
-        assert Case(**result[0])
+        # THEN a demo case should be found in the database
+        result = get_cases(db)
+        assert isinstance(result[0], Case)
