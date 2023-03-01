21010799eff85e19bc03b2c55099db1dcca874e0
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-01 09:21:19 +0100

    Add typing and import Dict

--- src/chanjo2/populate_demo.py
+++ src/chanjo2/populate_demo.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from chanjo2.crud.cases import create_db_case, get_case
 from chanjo2.crud.samples import create_sample_in_case
 from chanjo2.dbutil import get_session
@@ -9,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 db: sessionmaker = next(get_session())
 
 DEMO_CASE: Dict[str, str] = {"name": "internal_id", "display_name": "643594"}
-DEMO_SAMPLE = {
+DEMO_SAMPLE: Dict[str, str] = {
     "name": "ADM1059A2",
     "display_name": "NA12882",
     "case_name": DEMO_CASE["name"],
