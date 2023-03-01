3c47360ab8e3d6c328cb63215242bd54f5236e97
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-01 08:42:13 +0100

    Added some dots and moved the test

--- src/chanjo2/populate_demo.py
+++ src/chanjo2/populate_demo.py
@@ -18,19 +18,19 @@ DEMO_SAMPLE = {
 
 
 def load_demo_data() -> Case:
-    """Loads demo data into the database of a demo instance of Chanjo2"""
+    """Loads demo data into the database of a demo instance of Chanjo2."""
     load_demo_case()
     load_demo_sample()
     return get_case(db=db, case_name=DEMO_CASE["name"])
 
 
 def load_demo_case():
-    """Load a demo case into the database"""
+    """Load a demo case into the database."""
     case: CaseCreate = CaseCreate(**DEMO_CASE)
     create_db_case(db=db, case=case)
 
 
 def load_demo_sample():
-    """Add a sample to a demo case"""
+    """Add a sample to a demo case."""
     sample: SampleCreate = SampleCreate(**DEMO_SAMPLE)
     create_sample_in_case(db=db, sample=sample)
