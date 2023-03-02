2b02e5996f4c4e45efa83330ff354473cd8f1f78
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-02 11:19:12 +0100

    Update src/chanjo2/models/pydantic_models.py
    Co-authored-by: Henrik Stranneheim <henrik.stranneheim@scilifelab.se>

--- src/chanjo2/models/pydantic_models.py
+++ src/chanjo2/models/pydantic_models.py
@@ -6,7 +6,7 @@ from typing import Any, List, Optional
 import validators
 from pydantic import BaseModel, validator
 
-WRONG_COVERAGE_FILE_MSG = (
+WRONG_COVERAGE_FILE_MSG: str = (
     "Coverage_file_path must be either an existing local file path or a URL"
 )
 WRONG_BED_FILE_MSG: str = "Provided intervals files is not a valid BED file"
