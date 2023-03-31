268d27c871a6ad9a810abe1fa52939fdce8014e2
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-31 11:55:58 +0200

    Statement type hint

--- src/chanjo2/crud/intervals.py
+++ src/chanjo2/crud/intervals.py
@@ -5,19 +5,16 @@ from chanjo2.models.sql_models import Gene as SQLGene
 from sqlalchemy import delete
 from sqlalchemy.engine.cursor import CursorResult
 from sqlalchemy.orm import Session, query
+from sqlalchemy.sql.expression import Delete
 
 
-def delete_intervals_for_build(
-    db: Session, interval_type: Union[SQLGene], build: Builds
-) -> None:
+def delete_intervals_for_build(db: Session, interval_type: Union[SQLGene], build: Builds) -> None:
     """Delete intervals from a given table by specifying a genome build."""
-    statement = delete(interval_type).where(interval_type.build == build)
+    statement: Delete = delete(interval_type).where(interval_type.build == build)
     db.execute(statement)
 
 
-def count_intervals_for_build(
-    db: Session, interval_type: Union[SQLGene], build: Builds
-) -> int:
+def count_intervals_for_build(db: Session, interval_type: Union[SQLGene], build: Builds) -> int:
     """Count intervals in table by specifying a genome build."""
     return db.query(interval_type).where(interval_type.build == build).count()
 
@@ -51,9 +48,7 @@ def bulk_insert_genes(db: Session, gene_list: List[Gene]):
 def get_genes(db: Session, build: Builds, limit: int) -> List[Gene]:
     """Returns genes in the given genome build."""
     return (
-        _filter_intervals_by_build(
-            intervals=db.query(SQLGene), interval_type=SQLGene, build=build
-        )
+        _filter_intervals_by_build(intervals=db.query(SQLGene), interval_type=SQLGene, build=build)
         .limit(limit)
         .all()
     )
