4a9a47403092a423c3229c839af2704e6cd9e9f9
Chiara Rasi <rasi.chiara@gmail.com>
2023-04-14 07:39:47 +0200

    Added docstring

--- src/chanjo2/models/pydantic_models.py
+++ src/chanjo2/models/pydantic_models.py
@@ -14,6 +14,7 @@ class Builds(str, Enum):
 
     @staticmethod
     def get_enum_values() -> List[str]:
+        """Returns the values of the available genome builds"""
         return [member.value for member in Builds]
 
 
