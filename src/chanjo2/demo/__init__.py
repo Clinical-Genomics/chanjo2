3bc98ba4f605fa3b6583e5116d56a02840c4aff1
Chiara Rasi <rasi.chiara@gmacil.com>
2023-05-22 21:11:32 +0200

    Rename variable also where it is imported

--- src/chanjo2/demo/__init__.py
+++ src/chanjo2/demo/__init__.py
@@ -6,5 +6,5 @@ GENE_PANEL_FILE: str = "109_green.bed"
 D4_DEMO_FILE: str = "panelapp_109_example.d4"
 
 # Paths
-gene_panel_path = str(files(BASE_PATH).joinpath(gene_panel_file))
-d4_demo_path = str(files(BASE_PATH).joinpath(d4_demo_file))
+gene_panel_path = str(files(BASE_PATH).joinpath(GENE_PANEL_FILE))
+d4_demo_path = str(files(BASE_PATH).joinpath(D4_DEMO_FILE))
