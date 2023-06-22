8c84d088d413c473a4f3b64c8c125bc9ac7b8695
Chiara Rasi <rasi.chiara@gmacil.com>
2023-06-22 15:01:24 +0200

    Added a test

--- tests/src/chanjo2/endpoints/test_intervals.py
+++ tests/src/chanjo2/endpoints/test_intervals.py
@@ -61,6 +61,37 @@ def test_load_genes(
     assert Gene(**result[0])
 
 
+@pytest.mark.parametrize("build, path", BUILD_GENES_RESOURCE)
+def test_load_genes_from_file(
+    build: str,
+    path: str,
+    client: TestClient,
+    endpoints: Type,
+):
+    """Test the endpoint that adds genes to the database in a given genome build parsing them from a local file."""
+
+    # GIVEN a number of genes contained in the demo file
+    nr_genes: int = len(list(resource_lines(path))) - 1
+
+    # WHEN sending a request to the load_genes with genome build and path to the file containing the gene definitoons
+    response: Response = client.post(f"{endpoints.LOAD_GENES}{build}?file_path={path}")
+
+    # THEN it should return success
+    assert response.status_code == status.HTTP_200_OK
+
+    # THEN all the genes should be loaded
+    assert response.json()["detail"] == f"{nr_genes} genes loaded into the database"
+
+    # WHEN sending a request to the "genes" endpoint
+    response: Response = client.post(endpoints.GENES, json={"build": build})
+    assert response.status_code == status.HTTP_200_OK
+    result = response.json()
+    # THEN the expected number of genes should be returned
+    assert len(result) == nr_genes
+    # AND the gene should have the right format
+    assert Gene(**result[0])
+
+
 @pytest.mark.parametrize("build", Builds.get_enum_values())
 def test_genes_multiple_filters(
     build: str,
