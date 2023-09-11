ec0a9362315e5704300e62f151bdb2156790a3cf
Chiara Rasi <rasi.chiara@gmacil.com>
2023-09-11 09:57:11 +0200

    Wrote ine test

--- tests/src/chanjo2/meta/test_handle_report_contents.py
+++ tests/src/chanjo2/meta/test_handle_report_contents.py
@@ -1,5 +1,6 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
+from fastapi.testclient import TestClient
 from sqlalchemy.orm import sessionmaker
 
 from chanjo2.constants import DEFAULT_COMPLETENESS_LEVELS
@@ -7,43 +8,64 @@ from chanjo2.crud.intervals import get_genes
 from chanjo2.demo import DEMO_COVERAGE_QUERY_DATA as query
 from chanjo2.meta.handle_d4 import D4File
 from chanjo2.meta.handle_d4 import get_sample_interval_coverage
+from chanjo2.meta.handle_report_contents import get_report_level_completeness_rows
 from chanjo2.models.sql_models import Gene as SQLGene
+
+DEFAULT_COVERAGE_LEVEL = 20
 
 
 def test_get_report_level_completeness_rows(
-    session: sessionmaker, genomic_ids_per_build
+    demo_session: sessionmaker,
+    demo_client: TestClient,
+    genomic_ids_per_build: Dict[str, List],
 ):
     """Test the function that report coverage completeness stats at the default level."""
 
-    samples_d4_files: List[Tuple[str, D4File]] = [
-        (sample["name"], D4File(sample["coverage_file_path"]))
-        for sample in query["samples"]
-    ]
-    gene_symbols = genomic_ids_per_build[query["build"]]["hgnc_symbols"]
-    assert gene_symbols
-    genes: List[SQLGene] = get_genes(
-        db=session,
-        build=query["build"],
-        ensembl_ids=[],
-        hgnc_ids=[],
-        hgnc_symbols=gene_symbols,
-        limit=None,
-    )
-
-    assert genes
-    # Given complete coverage stats data for a demo report query:
-    samples_coverage_stats: Dict[str, List[GeneCoverage]] = {
-        sample: get_sample_interval_coverage(
-            db=session,
-            d4_file=d4_file,
-            genes=genes,
-            interval_type=SQLGene,
-            completeness_thresholds=DEFAULT_COMPLETENESS_LEVELS,
+    with demo_client:
+        # GIVEN a list of samples associated to a d4 file
+        samples_d4_files: List[Tuple[str, D4File]] = [
+            (sample["name"], D4File(sample["coverage_file_path"]))
+            for sample in query["samples"]
+        ]
+        # And a list of genes present in the database
+        gene_symbols = genomic_ids_per_build[query["build"]]["hgnc_symbols"]
+        genes: List[SQLGene] = get_genes(
+            db=demo_session,
+            build=query["build"],
+            ensembl_ids=[],
+            hgnc_ids=[],
+            hgnc_symbols=gene_symbols,
+            limit=None,
         )
-        for sample, d4_file in samples_d4_files
-    }
+        assert genes
 
-    assert samples_coverage_stats == "jsk"
+        # GIVEN coverage stats data on these genes:
+        samples_coverage_stats: Dict[str, List[GeneCoverage]] = {
+            sample: get_sample_interval_coverage(
+                db=demo_session,
+                d4_file=d4_file,
+                genes=genes,
+                interval_type=SQLGene,
+                completeness_thresholds=DEFAULT_COMPLETENESS_LEVELS,
+            )
+            for sample, d4_file in samples_d4_files
+        }
+        assert samples_coverage_stats
+
+        # THEN it should be possible to retrieve stats for the default coverage level:
+        default_level_samples_coverage_stats: List[
+            Tuple[str, float, str]
+        ] = get_report_level_completeness_rows(
+            samples_coverage_stats=samples_coverage_stats, level=DEFAULT_COVERAGE_LEVEL
+        )
+        for (
+            sample,
+            mean_cov_intervals,
+            not_covered_intervals,
+        ) in default_level_samples_coverage_stats:
+            assert isinstance(sample, str)
+            assert isinstance(mean_cov_intervals, float) or mean_cov_intervals == 0
+            assert isinstance(not_covered_intervals, str)
 
 
 def test_get_report_completeness_rows(session: sessionmaker):
