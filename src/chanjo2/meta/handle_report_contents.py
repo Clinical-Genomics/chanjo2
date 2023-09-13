ee2f5627c1bfd03dedb1fad2e5118c7ed8440b8f
Chiara Rasi <rasi.chiara@gmacil.com>
2023-09-13 10:25:44 +0200

    Coverage metrics explanation on coverage report

--- src/chanjo2/meta/handle_report_contents.py
+++ src/chanjo2/meta/handle_report_contents.py
@@ -72,6 +72,7 @@ def get_report_data(query: ReportQuery, session: Session) -> Dict:
         "extras": {
             "panel_name": query.panel_name,
             "default_level": query.default_level,
+            "interval_type": query.interval_type.value,
         },
         "sex_rows": get_report_sex_rows(
             samples=query.samples, samples_d4_files=samples_d4_files
