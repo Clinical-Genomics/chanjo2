3b2a4cf9d08f741b35c05e047d26b5b0eefb1d74
Chiara Rasi <rasi.chiara@gmacil.com>
2023-09-15 15:16:54 +0200

    Simplify

--- src/chanjo2/meta/handle_overview_content.py
+++ src/chanjo2/meta/handle_overview_content.py
@@ -2,7 +2,6 @@ from typing import Dict
 
 from sqlmodel import Session
 
-from chanjo2.meta.handle_report_contents import get_ordered_levels
 from chanjo2.models.pydantic_models import GeneralReportQuery
 
 
@@ -10,9 +9,7 @@ def get_overview_data(query: GeneralReportQuery, session: Session) -> Dict:
     """Return the information that will be displayed in the coverage overview page."""
 
     data: Dict = {
-        "levels": get_ordered_levels(threshold_levels=query.completeness_thresholds),
         "extras": {
-            "default_level": query.default_level,
             "interval_type": query.interval_type.value,
             "samples": [
                 {"name": sample.name, "coverage_file_path": sample.coverage_file_path}
