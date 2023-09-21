733c6e9bda7ebbc04e4a626cbdfa85b7dcaef362
Chiara Rasi <rasi.chiara@gmacil.com>
2023-09-21 15:53:22 +0200

    Work in progress

--- src/chanjo2/endpoints/overview.py
+++ src/chanjo2/endpoints/overview.py
@@ -1,9 +1,11 @@
 from os import path
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlmodel import Session
 
+from chanjo2.dbutil import get_session
 from chanjo2.demo import DEMO_OVERVIEW_QUERY_DATA
 from chanjo2.meta.handle_overview_content import get_overview_data
 from chanjo2.models.pydantic_models import GeneralReportQuery
@@ -20,15 +22,17 @@ router = APIRouter()
 
 
 @router.get("/overview/demo", response_class=HTMLResponse)
-async def demo_overview(request: Request):
+async def demo_overview(request: Request, db: Session = Depends(get_session)):
     """Return a demo genes overview page over a list of genes for a list of samples."""
 
     overview_query = GeneralReportQuery(**DEMO_OVERVIEW_QUERY_DATA)
-    overview_content: dict = get_overview_data(query=overview_query)
+    overview_content: dict = get_overview_data(query=overview_query, session=db)
     return templates.TemplateResponse(
         "overview.html",
         {
             "request": request,
             "extras": overview_content["extras"],
+            "levels": overview_content["levels"],
+            "samples_coverage_stats": overview_content["samples_coverage_stats"]
         },
     )
--- src/chanjo2/meta/handle_overview_content.py
+++ src/chanjo2/meta/handle_overview_content.py
@@ -1,10 +1,41 @@
-from typing import Dict
+import logging
 
+from pyd4 import D4File
+from sqlmodel import Session
+
+from chanjo2.crud.intervals import get_genes
+from chanjo2.meta.handle_d4 import get_sample_interval_coverage
+from chanjo2.meta.handle_report_contents import get_ordered_levels, set_samples_coverage_files, INTERVAL_TYPE_SQL_TYPE
 from chanjo2.models.pydantic_models import GeneralReportQuery
 
+LOG = logging.getLogger("uvicorn.access")
 
-def get_overview_data(query: GeneralReportQuery) -> dict:
+
+def get_overview_data(query: GeneralReportQuery, session: Session) -> dict:
     """Return the information that will be displayed in the coverage overview page."""
+
+    set_samples_coverage_files(session=session, samples=query.samples)
+    samples_d4_files: List[Tuple[str, D4File]] = [
+        (sample.name, D4File(sample.coverage_file_path)) for sample in query.samples
+    ]
+    genes: List[SQLGene] = get_genes(
+        db=session,
+        build=query.build,
+        ensembl_ids=query.ensembl_gene_ids,
+        hgnc_ids=query.hgnc_gene_ids,
+        hgnc_symbols=query.hgnc_gene_symbols,
+        limit=None,
+    )
+    samples_coverage_stats: Dict[str, List[GeneCoverage]] = {
+        sample: get_sample_interval_coverage(
+            db=session,
+            d4_file=d4_file,
+            genes=genes,
+            interval_type=INTERVAL_TYPE_SQL_TYPE[query.interval_type],
+            completeness_thresholds=query.completeness_thresholds,
+        )
+        for sample, d4_file in samples_d4_files
+    }
 
     return {
         "extras": {
@@ -14,4 +45,6 @@ def get_overview_data(query: GeneralReportQuery) -> dict:
                 for sample in query.samples
             ],
         },
+        "levels": get_ordered_levels(query.completeness_thresholds),
+        "samples_coverage_stats": samples_coverage_stats
     }
