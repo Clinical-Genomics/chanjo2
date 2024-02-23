e2f0c510e09bc9148f74101b366e18b104efedfc
Chiara Rasi <rasi.chiara@gmacil.com>
2024-02-23 19:35:59 +0100

    Fix bug

--- src/chanjo2/meta/handle_report_contents.py
+++ src/chanjo2/meta/handle_report_contents.py
@@ -230,11 +230,16 @@ def get_report_completeness_rows(
 
         completeness_row: Dict[str, float] = {}
         for completeness_key, completeness_values in sample_stats.items():
-            completeness_row[completeness_key] = (
-                round((mean(completeness_values) * 100), 2)
-                if completeness_values
-                else 0
-            )
+            if completeness_key != "mean_coverage":
+                completeness_row[completeness_key] = (
+                    round((mean(completeness_values) * 100), 2)
+                    if completeness_values
+                    else 0
+                )
+            else:  # Do not return percentage for mean coverage
+                completeness_row[completeness_key] = (
+                    round((mean(completeness_values)), 2) if completeness_values else 0
+                )
 
         completeness_rows.append((sample, completeness_row))
 
