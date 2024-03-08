f3ed1d19db50b5a8d9f32d43b9fc3130a917a7a1
Chiara Rasi <rasi.chiara@gmacil.com>
2024-03-08 14:09:15 +0100

    /Users/chiararasi/Documents/work/GITs/chanjo2/src/chanjo2/meta/handle_report_contents.py

--- src/chanjo2/meta/handle_report_contents.py
+++ src/chanjo2/meta/handle_report_contents.py
@@ -6,14 +6,21 @@ from sqlalchemy.orm import Session
 
 from chanjo2.crud.intervals import get_genes, get_hgnc_gene, set_sql_intervals
 from chanjo2.crud.samples import get_sample
-from chanjo2.meta.handle_d4 import (get_report_sample_interval_coverage,
-                                    get_sample_interval_coverage,
-                                    get_samples_sex_metrics)
+from chanjo2.meta.handle_d4 import (
+    get_report_sample_interval_coverage,
+    get_sample_interval_coverage,
+    get_samples_sex_metrics,
+)
 from chanjo2.models import SQLExon, SQLGene, SQLSample, SQLTranscript
-from chanjo2.models.pydantic_models import (GeneCoverage, GeneReportForm,
-                                            IntervalType, ReportQuery,
-                                            ReportQuerySample, SampleSexRow,
-                                            TranscriptTag)
+from chanjo2.models.pydantic_models import (
+    GeneCoverage,
+    GeneReportForm,
+    IntervalType,
+    ReportQuery,
+    ReportQuerySample,
+    SampleSexRow,
+    TranscriptTag,
+)
 
 LOG = logging.getLogger("uvicorn.access")
 
