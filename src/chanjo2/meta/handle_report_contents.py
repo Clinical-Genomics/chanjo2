d4a4eea0825ed05f5aaee1d3a393670604b3e9ce
Chiara Rasi <rasi.chiara@gmacil.com>
2024-03-28 13:51:12 +0100

    Fix typo

--- src/chanjo2/meta/handle_report_contents.py
+++ src/chanjo2/meta/handle_report_contents.py
@@ -64,7 +64,7 @@ def _serialize_sample(sample: ReportQuerySample) -> Dict[str, str]:
 
 
 def get_report_data(
-    query: ReportQuery, session: Session, is_overview: Optional[boolean] = False
+    query: ReportQuery, session: Session, is_overview: Optional[bool] = False
 ) -> Dict:
     """Return the information that will be displayed in the coverage report or in the genes overview report.."""
 
