1067f83b44107f33a43ecf0706742857d708b09f
Chiara Rasi <rasi.chiara@gmacil.com>
2024-08-02 09:04:04 +0200

    Replaced deprecated Pydantic  method with

--- tests/src/chanjo2/meta/test_handle_report_contents.py
+++ tests/src/chanjo2/meta/test_handle_report_contents.py
@@ -3,10 +3,8 @@ from typing import List, Tuple
 from sqlalchemy.orm import sessionmaker
 
 from chanjo2.demo import DEMO_COVERAGE_QUERY_DATA
-from chanjo2.meta.handle_report_contents import (
-    get_missing_genes_from_db,
-    get_report_data,
-)
+from chanjo2.meta.handle_report_contents import (get_missing_genes_from_db,
+                                                 get_report_data)
 from chanjo2.models import SQLGene
 from chanjo2.models.pydantic_models import ReportQuery
 
@@ -49,7 +47,7 @@ def test_get_report_data(demo_session: sessionmaker):
     """Test the function that collects the deta required for creating a coverage/overview report."""
 
     # GIVEN a user query containing the expected parameters
-    query = ReportQuery.parse_obj(DEMO_COVERAGE_QUERY_DATA)
+    query = ReportQuery.model_validate(DEMO_COVERAGE_QUERY_DATA)
     report_data: dict = get_report_data(query=query, session=demo_session)
 
     # THEN get_report_data should return a dictionary with the expected report info
