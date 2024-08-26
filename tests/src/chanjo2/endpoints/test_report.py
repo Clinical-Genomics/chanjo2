1e2fa3c8564b1c011f2f16fa14242d772b7a19fc
Chiara Rasi <rasi.chiara@gmacil.com>
2024-08-26 15:28:38 +0200

    Remove a test

--- tests/src/chanjo2/endpoints/test_report.py
+++ tests/src/chanjo2/endpoints/test_report.py
@@ -1,11 +1,9 @@
-from copy import deepcopy
 from typing import Type
 
 from fastapi import status
 from fastapi.testclient import TestClient
 from requests.models import Response
 
-from chanjo2.constants import GENE_LISTS_NOT_SUPPORTED_MSG
 from chanjo2.demo import DEMO_COVERAGE_QUERY_FORM
 
 
@@ -20,26 +18,6 @@ def test_demo_report(client: TestClient, endpoints: Type):
 
     # And return an HTML page
     assert response.template.name == "report.html"
-
-
-def test_report_request_no_genes(client: TestClient, endpoints: Type):
-    """Test error handling when user tries to create a coverage report without providing a list of genes."""
-
-    # GIVEN a request without a list of genes of interest
-    REPORT_FORM_NO_GENES: dict = deepcopy(DEMO_COVERAGE_QUERY_FORM)
-    REPORT_FORM_NO_GENES.pop("hgnc_gene_symbols")
-
-    # GIVEN a query with json data to the report endpoint
-    response: Response = client.post(
-        endpoints.REPORT,
-        data=REPORT_FORM_NO_GENES,
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-    )
-
-    # THEN the response should return the expected error code and message
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    result = response.json()
-    assert GENE_LISTS_NOT_SUPPORTED_MSG in result["detail"]
 
 
 def test_report_form_data(client: TestClient, endpoints: Type):
