2682682a72a20b8638b3a8e1a93cc2c7fa753ecc
Chiara Rasi <rasi.chiara@gmacil.com>
2024-09-24 15:21:34 +0200

    Add a dedicated test

--- tests/src/chanjo2/endpoints/test_report.py
+++ tests/src/chanjo2/endpoints/test_report.py
@@ -34,3 +34,26 @@ def test_report_form_data(client: TestClient, endpoints: Type):
 
     # And return an HTML page
     assert response.template.name == "report.html"
+
+
+def test_report_form_data_no_genes(client: TestClient, endpoints: Type):
+    """Test the coverage report endpoint by providing application/x-www-form-urlencoded data in a POST request.
+    Make sure that report gets creates even if request is missing genes info."""
+
+    # GIVEN a submitted form not containing any genes:
+    coverage_query_form_no_genes = dict(DEMO_COVERAGE_QUERY_FORM)
+    for key in ["ensembl_gene_ids", "hgnc_gene_ids", "hgnc_gene_symbols"]:
+        coverage_query_form_no_genes.pop(key)
+
+    # GIVEN a query with application/x-www-form-urlencoded data to the report endpoint
+    response: Response = client.post(
+        endpoints.REPORT,
+        data=coverage_query_form_no_genes,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+
+    # Then the request should be successful
+    assert response.status_code == status.HTTP_200_OK
+
+    # And return an HTML page
+    assert response.template.name == "report.html"
