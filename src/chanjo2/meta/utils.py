34e0549bd67ad0fe183bae67044b85fb38fc9f5c
Chiara Rasi <rasi.chiara@gmacil.com>
2025-10-02 09:13:10 +0200

    Fix handle ind

--- src/chanjo2/meta/utils.py
+++ src/chanjo2/meta/utils.py
@@ -5,7 +5,8 @@ from typing import List, Union
 
 def get_mean(float_list: List[float], round_by: int = 2) -> Union[float, str]:
     """Return the mean value from a list of floats, optionally rounded.
-    Returns 'NA' if the list has no valid numbers."""
+    Returns 'NA' if the list has no valid numbers.
+    Converts inf/-inf to string 'inf'/'-inf'."""
 
     # Filter out NaNs
     clean_list = [x for x in float_list if not math.isnan(x)]
@@ -16,7 +17,11 @@ def get_mean(float_list: List[float], round_by: int = 2) -> Union[float, str]:
     mean_value = mean(clean_list)
 
     # Round only if round_by is not None
-    if round_by is not None:
+    if round_by is not None and math.isfinite(mean_value):
         mean_value = round(mean_value, round_by)
+
+    # Convert infinities to strings
+    if math.isinf(mean_value):
+        return str(mean_value)
 
     return mean_value
